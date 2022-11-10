53b47b250c452103cc27592144e497998ad891b6
Ángel Sánchez Corchado <ansancor15@gmail.com>
2022-11-10 20:44:24 +0100

    Diseño de la vista MainView
    Creación de la página principal de la web con anuncios y banner de inicio de BIWAN

--- src/main/java/es/uca/iw/biwan/views/main/MainView.java
+++ src/main/java/es/uca/iw/biwan/views/main/MainView.java
@@ -1,5 +1,6 @@
 package es.uca.iw.biwan.views.main;
 
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.*;
@@ -17,27 +18,27 @@ import java.time.format.TextStyle;
 @CssImport("/themes/biwan/main.css")
 public class MainView extends VerticalLayout {
     public MainView() {
-        Image img1 = new Image("images/3.png", "Imagen");
-        img1.setWidth("90%");
-        img1.setHeight("500px");
+        Image img = new Image("images/banner.png", "Imagen");
+        img.setWidth("90%");
+        img.setMaxHeight("450px");
 
-        Image img2 = new Image("images/3.png", "Imagen");
-        img2.setWidth("90%");
-        img2.setHeight("500px");
+        Image img1 = new Image("images/1.jpg", "Imagen");
+        img1.setMaxHeight("500px");
+        img1.setMaxWidth("500px");
 
-        Image img3 = new Image("images/3.png", "Imagen");
-        img3.setWidth("90%");
-        img3.setHeight("500px");
+        Image img2 = new Image("images/2.jpg", "Imagen");
+        img2.setMaxHeight("500px");
+        img2.setMaxWidth("500px");
 
-        Image img4 = new Image("images/3.png", "Imagen");
-        img4.setWidth("90%");
-        img4.setHeight("500px");
+        Image img3 = new Image("images/3.jpg", "Imagen");
+        img3.setMaxHeight("500px");
+        img3.setMaxWidth("500px");
 
-        Anchor BotonHazteCliente1 = new Anchor("", "Hazte cliente");
+        Anchor BotonHazteCliente1 = new Anchor("registration", "Hazte cliente");
         BotonHazteCliente1.addClassName("BotonHazteCliente1");
-        Anchor BotonHazteCliente2 = new Anchor("", "Hazte cliente");
+        Anchor BotonHazteCliente2 = new Anchor("registration", "Hazte cliente");
         BotonHazteCliente2.addClassName("BotonHazteCliente2");
-        Anchor BotonHazteCliente3 = new Anchor("", "Hazte cliente");
+        Anchor BotonHazteCliente3 = new Anchor("registration", "Hazte cliente");
         BotonHazteCliente3.addClassName("BotonHazteCliente3");
 
         Anchor BotonInfo1 = new Anchor("", "Más información");
@@ -47,65 +48,73 @@ public class MainView extends VerticalLayout {
         Anchor BotonInfo3 = new Anchor("", "Más información");
         BotonInfo3.addClassName("BotonInfo3");
 
-        Button TextoPrincipal = new Button("¡¡Bienvenido a BIWAN!!, tu gestor de banca online de confianza");
-        TextoPrincipal.addClassName("TextoPrincipal");
+        var TituloTextoAnuncio1 = new H3 ("Hazte cliente de BIWAN ahora y ayuda al medio ambiente");
+        TituloTextoAnuncio1.addClassName("TituloTextoAnuncio1");
+        var TextoAnuncio1 = new H5 ("Los árboles son fuente de vida. No solo en referencia a los ecosistemas naturales, " +
+                "sino también para la supervivencia del ser humano. Su uso para alimentarse, calentarse y construir un sinfín " +
+                "de objetos supone una explotación que, entre otros factores, dispara la deforestación y, con ella, " +
+                "la destrucción del hábitat. Cada vez que un cliente llega a BIWAN, nosotros plantamos un árbol. " +
+                "Estamos comprometidos contigo pero también con el medio ambiente. Por eso, y muchas otras cosas, " +
+                "te invitamos a que te uns a nosotros.");
+        TextoAnuncio1.addClassName("TextoAnuncio1");
 
-        var vlAnuncio1 = new VerticalLayout(TextoPrincipal, img1);
-        vlAnuncio1.setAlignItems(Alignment.CENTER);
+        var TituloTextoAnuncio2 = new H3 ("Gracias al servicio de nuestros Gestores haremos todo por ti");
+        TituloTextoAnuncio2.addClassName("TituloTextoAnuncio2");
+        var TextoAnuncio2 = new H5 ("¿No tienes tiempo para gestionar tus cuentas y tarjetas? ¿No sabes cómo hacerlo? " +
+                "¿No sabes qué hacer si te llega una factura? BIWAN te ofrece un servicio de gestión de tus cuentas y tarjetas " +
+                "para que no tengas que preocuparte de nada. Nosotros nos encargamos de gestionarlo todo, " +
+                "y de hacer las gestiones necesarias para que tu no tengas que preocuparte de nada. " +
+                "Además, te ofrecemos un servicio de atención al cliente para que puedas resolver cualquier duda " +
+                "que tengas sobre tus cuentas y tarjetas, las cuales seran respondidas por nuestros grandes gestores.");
+        TextoAnuncio2.addClassName("TextoAnuncio2");
 
-        var vlAnuncio2 = new VerticalLayout(img2);
-        vlAnuncio2.setAlignItems(Alignment.CENTER);
-        var TextoAnuncio2 = new VerticalLayout(new H6("Texto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemplo"),
-                new HorizontalLayout(BotonHazteCliente1, BotonInfo1));
+        var TituloTextoAnuncio3 = new H3 ("Llévate hasta 150 € con el Plan Invita a un Amigo");
+        TituloTextoAnuncio3.addClassName("TituloTextoAnuncio3");
+        var TextoAnuncio3 = new H5 ("¿Quieres llevarte 15 € por cada amigo que invites a BIWAN (máximo 10 amigos)" +
+                " y conseguir que ellos se lleven también 15 €?" +
+                " Solo tienes que seguir estos pasos: 1) Hazte cliente con la Cuenta Online Sin Comisiones." +
+                "Accede al área privada de cliente en \"Mis promociones\" y comparte tu código con amigos y familiares." +
+                "2) Para llevaros el premio, tus amigos tendrán que utilizar tu código al hacerse clientes y realizar una " +
+                "compra superior a 15 € con su nueva tarjeta BIWAN. Consulta las condiciones de la promoción en el apartado " +
+                "\"más información\".");
+        TextoAnuncio3.addClassName("TextoAnuncio3");
 
-        var vlAnuncio3 = new VerticalLayout(img3);
-        vlAnuncio3.setAlignItems(Alignment.CENTER);
-        var TextoAnuncio3 = new VerticalLayout(new H6("Texto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemplo"),
-                new HorizontalLayout(BotonHazteCliente2, BotonInfo2));
+        var vlAnuncioPrincipal = new VerticalLayout(img);
+        vlAnuncioPrincipal.setAlignItems(Alignment.CENTER);
 
-        var vlAnuncio4 = new VerticalLayout(img4);
-        vlAnuncio4.setAlignItems(Alignment.CENTER);
-        var TextoAnuncio4 = new VerticalLayout(new H6("Texto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de " +
-                "ploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemploTexto de ejemplo"),
-                new HorizontalLayout(BotonHazteCliente3, BotonInfo3));
+        var vlImagenAnuncio1 = new VerticalLayout(img1);
+        vlImagenAnuncio1.setAlignItems(Alignment.END);
+        var vlTextoAnuncio1 = new VerticalLayout(TituloTextoAnuncio1, TextoAnuncio1, new HorizontalLayout(BotonHazteCliente1, BotonInfo1));
 
-        var hlAnuncio1 = new HorizontalLayout(vlAnuncio1);
+        var vlImagenAnuncio2 = new VerticalLayout(img2);
+        vlImagenAnuncio2.setAlignItems(Alignment.START);
+        var vlTextoAnuncio2 = new VerticalLayout(TituloTextoAnuncio2, TextoAnuncio2, new HorizontalLayout(BotonHazteCliente2, BotonInfo2));
+
+        var vlImagenAnuncio3 = new VerticalLayout(img3);
+        vlImagenAnuncio3.setAlignItems(Alignment.END);
+        var vlTextoAnuncio3 = new VerticalLayout(TituloTextoAnuncio3, TextoAnuncio3, new HorizontalLayout(BotonHazteCliente3, BotonInfo3));
+
+        var hlAnuncioPrincipal = new HorizontalLayout(vlAnuncioPrincipal);
+        hlAnuncioPrincipal.setWidthFull();
+        hlAnuncioPrincipal.addClassName("hlAnuncioPrincipal");
+
+        var hlAnuncio1 = new HorizontalLayout(vlTextoAnuncio1, vlImagenAnuncio1);
         hlAnuncio1.setWidthFull();
+        hlAnuncio1.addClassName("hlAnuncio1");
 
-        var hlAnuncio2 = new HorizontalLayout(TextoAnuncio2, vlAnuncio2);
+        var hlAnuncio2 = new HorizontalLayout(vlImagenAnuncio2, vlTextoAnuncio2);
         hlAnuncio2.setWidthFull();
+        hlAnuncio2.addClassName("hlAnuncio2");
 
-        var hlAnuncio3 = new HorizontalLayout(vlAnuncio3, TextoAnuncio3);
+        var hlAnuncio3 = new HorizontalLayout(vlTextoAnuncio3, vlImagenAnuncio3);
         hlAnuncio3.setWidthFull();
-
-        var hlAnuncio4 = new HorizontalLayout(TextoAnuncio4, vlAnuncio4);
-        hlAnuncio4.setWidthFull();
+        hlAnuncio3.addClassName("hlAnuncio3");
 
         add(HeaderView.Header());
+        add(hlAnuncioPrincipal);
         add(hlAnuncio1);
         add(hlAnuncio2);
         add(hlAnuncio3);
-        add(hlAnuncio4);
         add(FooterView.Footer());
     }
 
