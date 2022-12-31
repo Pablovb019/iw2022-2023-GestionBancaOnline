86241cbb31c7eb835665b8ad2c5ee079889b6fd2
Jose Antonio Marañón Calvario <jose.maranoncalvario@alum.uca.es>
2022-12-31 19:05:37 +0100

    Ajuste GestorView
    Añadido ajuste para que a los gestores le aparezca solo los clientes asignados

--- src/main/java/es/uca/iw/biwan/views/usuarios/GestorView.java
+++ src/main/java/es/uca/iw/biwan/views/usuarios/GestorView.java
@@ -31,6 +31,7 @@ import es.uca.iw.biwan.views.headers.HeaderUsuarioLogueadoView;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 @CssImport("./themes/biwan/paginaPrincipalGestor.css")
 @PageTitle("Página Principal Gestor")
@@ -95,79 +96,81 @@ public class GestorView extends VerticalLayout {
 
         ArrayList<Component> MenuPorCliente = new ArrayList<>();
         for(Usuario cliente : clientes) {
-            HorizontalLayout layoutComponenteTabla = new HorizontalLayout();
-            Anchor NombreCliente = new Anchor("");
-            NombreCliente.setText(cliente.getNombre() + " " + cliente.getApellidos());
-            Anchor CuentasYTarjetasButton = new Anchor("cuentas-tarjetas-gestor", "Cuentas y tarjetas");
+            if(Objects.equals(cliente.getGestor_id(), session.getAttribute(Gestor.class).getUUID())) {
+                HorizontalLayout layoutComponenteTabla = new HorizontalLayout();
+                Anchor NombreCliente = new Anchor("");
+                NombreCliente.setText(cliente.getNombre() + " " + cliente.getApellidos());
+                Anchor CuentasYTarjetasButton = new Anchor("cuentas-tarjetas-gestor", "Cuentas y tarjetas");
 
-            CuentasYTarjetasButton.getElement().addEventListener("click", event -> {
-                cuentasTarjetasGestorView.cliente = (Cliente) cliente;
-            });
-            Button CrearCuentaButton = new Button("Crear cuenta");
-            CrearCuentaButton.getElement().getStyle().set("cursor", "pointer");
+                CuentasYTarjetasButton.getElement().addEventListener("click", event -> {
+                    cuentasTarjetasGestorView.cliente = (Cliente) cliente;
+                });
+                Button CrearCuentaButton = new Button("Crear cuenta");
+                CrearCuentaButton.getElement().getStyle().set("cursor", "pointer");
 
-            Button CrearTarjetaButton = new Button("Crear tarjeta");
-            CrearTarjetaButton.getElement().getStyle().set("cursor", "pointer");
+                Button CrearTarjetaButton = new Button("Crear tarjeta");
+                CrearTarjetaButton.getElement().getStyle().set("cursor", "pointer");
 
-            CrearCuentaButton.addClickListener(event -> {
-                Cuenta nuevaCuenta = new Cuenta();
-                cuentaService.save(nuevaCuenta, (Cliente) cliente);
-                new ConfirmDialog("Cuenta creada", "La cuenta ha sido creada correctamente", "Aceptar", null).open();
-            });
-
-            CrearTarjetaButton.addClickListener(event -> {
-                Dialog dialog = new Dialog();
-                dialog.setHeaderTitle("Crear tarjeta");
-
-                VerticalLayout dialogLayout = new VerticalLayout();
-                ComboBox<Cuenta> cuentas = new ComboBox<>("Cuentas");
-                cuentas.setItems(cuentaService.findCuentaByCliente(cliente));
-                cuentas.setItemLabelGenerator(Cuenta::getIBAN);
-                cuentas.setRequired(true);
-                cuentas.setRequiredIndicatorVisible(true);
-
-                Button crearTarjeta = new Button("Crear tarjeta");
-                crearTarjeta.getElement().getStyle().set("cursor", "pointer");
-                crearTarjeta.addClickListener(event1 -> {
-                    Tarjeta nuevaTarjeta = new Tarjeta();
-                    tarjetaService.save(nuevaTarjeta, cuentas.getValue());
-                    new ConfirmDialog("Tarjeta creada", "La tarjeta ha sido creada correctamente", "Aceptar", null).open();
-                    dialog.close();
+                CrearCuentaButton.addClickListener(event -> {
+                    Cuenta nuevaCuenta = new Cuenta();
+                    cuentaService.save(nuevaCuenta, (Cliente) cliente);
+                    new ConfirmDialog("Cuenta creada", "La cuenta ha sido creada correctamente", "Aceptar", null).open();
                 });
 
-                dialogLayout.add(cuentas, crearTarjeta);
-                dialog.add(dialogLayout);
-                dialog.open();
-            });
+                CrearTarjetaButton.addClickListener(event -> {
+                    Dialog dialog = new Dialog();
+                    dialog.setHeaderTitle("Crear tarjeta");
 
-            Anchor ConsultaOnlineButton = new Anchor("consultas-online-gestor", "Consulta Online");
-            Span counterOnline = new Span("1");
-            Anchor ConsultaOfflineButton = new Anchor("consultas-offline-gestor", "Consulta Offline");
-            Span counterOffline = new Span("3");
-            ConsultaOfflineButton.getElement().addEventListener("click", event -> {
-                _cliente = (Cliente) cliente;
-            });
+                    VerticalLayout dialogLayout = new VerticalLayout();
+                    ComboBox<Cuenta> cuentas = new ComboBox<>("Cuentas");
+                    cuentas.setItems(cuentaService.findCuentaByCliente(cliente));
+                    cuentas.setItemLabelGenerator(Cuenta::getIBAN);
+                    cuentas.setRequired(true);
+                    cuentas.setRequiredIndicatorVisible(true);
 
-            //ADD CLASS
-            NombreCliente.addClassNames("NombreClienteAnchor", "Separacion");
-            CuentasYTarjetasButton.addClassNames("Separacion", "AnchorButton");
-            CrearCuentaButton.addClassNames("Separacion", "BotonGestor");
-            CrearTarjetaButton.addClassNames("Separacion", "BotonGestor");
-            ConsultaOnlineButton.addClassNames("Separacion", "BotonGestor");
-            ConsultaOfflineButton.addClassNames("Separacion", "BotonGestor");
-            layoutComponenteTabla.addClassName("layoutGestionCliente");
-            counterOnline.addClassName("counter");
-            counterOffline.addClassName("counter");
+                    Button crearTarjeta = new Button("Crear tarjeta");
+                    crearTarjeta.getElement().getStyle().set("cursor", "pointer");
+                    crearTarjeta.addClickListener(event1 -> {
+                        Tarjeta nuevaTarjeta = new Tarjeta();
+                        tarjetaService.save(nuevaTarjeta, cuentas.getValue());
+                        new ConfirmDialog("Tarjeta creada", "La tarjeta ha sido creada correctamente", "Aceptar", null).open();
+                        dialog.close();
+                    });
 
-            //ALIGNMENT
-            layoutComponenteTabla.expand(NombreCliente);
-            layoutComponenteTabla.setJustifyContentMode(JustifyContentMode.BETWEEN);
+                    dialogLayout.add(cuentas, crearTarjeta);
+                    dialog.add(dialogLayout);
+                    dialog.open();
+                });
 
-            //ADD
-            ConsultaOnlineButton.add(counterOnline);
-            ConsultaOfflineButton.add(counterOffline);
-            layoutComponenteTabla.add(NombreCliente, CuentasYTarjetasButton, CrearCuentaButton, CrearTarjetaButton, ConsultaOnlineButton, ConsultaOfflineButton);
-            MenuPorCliente.add(layoutComponenteTabla);
+                Anchor ConsultaOnlineButton = new Anchor("consultas-online-gestor", "Consulta Online");
+                Span counterOnline = new Span("1");
+                Anchor ConsultaOfflineButton = new Anchor("consultas-offline-gestor", "Consulta Offline");
+                Span counterOffline = new Span("3");
+                ConsultaOfflineButton.getElement().addEventListener("click", event -> {
+                    _cliente = (Cliente) cliente;
+                });
+
+                //ADD CLASS
+                NombreCliente.addClassNames("NombreClienteAnchor", "Separacion");
+                CuentasYTarjetasButton.addClassNames("Separacion", "AnchorButton");
+                CrearCuentaButton.addClassNames("Separacion", "BotonGestor");
+                CrearTarjetaButton.addClassNames("Separacion", "BotonGestor");
+                ConsultaOnlineButton.addClassNames("Separacion", "BotonGestor");
+                ConsultaOfflineButton.addClassNames("Separacion", "BotonGestor");
+                layoutComponenteTabla.addClassName("layoutGestionCliente");
+                counterOnline.addClassName("counter");
+                counterOffline.addClassName("counter");
+
+                //ALIGNMENT
+                layoutComponenteTabla.expand(NombreCliente);
+                layoutComponenteTabla.setJustifyContentMode(JustifyContentMode.BETWEEN);
+
+                //ADD
+                ConsultaOnlineButton.add(counterOnline);
+                ConsultaOfflineButton.add(counterOffline);
+                layoutComponenteTabla.add(NombreCliente, CuentasYTarjetasButton, CrearCuentaButton, CrearTarjetaButton, ConsultaOnlineButton, ConsultaOfflineButton);
+                MenuPorCliente.add(layoutComponenteTabla);
+            }
         }
 
         //ADD CLASS
