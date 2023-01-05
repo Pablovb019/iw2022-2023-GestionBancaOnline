35f59662bef45cad5df03eb97b8edf42f5d675ff
Pablo Vilar Bustillo <pablovb01@gmail.com>
2023-01-05 09:51:58 +0100

    Preparación del Collaboration Engine para fuera de Production

--- src/main/java/es/uca/iw/biwan/JpaApplication.java
+++ src/main/java/es/uca/iw/biwan/JpaApplication.java
@@ -2,16 +2,12 @@ package es.uca.iw.biwan;
 
 import com.vaadin.collaborationengine.CollaborationEngineConfiguration;
 import com.vaadin.flow.component.dependency.NpmPackage;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
-import es.uca.iw.biwan.domain.rol.Role;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -34,8 +30,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 
 public class JpaApplication implements AppShellConfigurator {
+
+    @Bean
+    public CollaborationEngineConfiguration ceConfigBean() {
+        CollaborationEngineConfiguration configuration = new CollaborationEngineConfiguration(
+                licenseEvent -> {
+                    // See <<ce.production.license-events>>
+                });
+        configuration.setDataDir(System.getenv("VAADIN_CE_DATA_DIR"));
+        return configuration;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(JpaApplication.class, args);
     }
-
 }
